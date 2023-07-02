8d0293ef8ecd07aef1070b27e36a308518963391
piinniitta <panita3824@gmail.com>
2023-07-02 22:29:14 +0700

    initial commit and add index.php,index.css
    create index.php and index.css

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <div class="container" style="max-width: 1400px;">
+            <nav>
+                <div class="logo">
+                    <img src="img/Group 311.svg" class="logo">
+                </div>
+                <h4 class="webname">NU BICYCLE</h4>
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="About.php">ABOUT</a></li>
+                </ul>
+                <div class="nav-btn">
+                    <img src="img/carbon_login.svg">
+                    <a href="signUp.php" class="login">Login</a>
+                </div>
+            </nav>
+            <section class="header-info">
+                <div class="circle"></div>
+                <div class="content">
+                    <div class="textBox">
+                        <div class="header-title">
+                            <h2>BICYCLE </h2>
+                            <h2>BORROWING-RETURNING </h2>
+                            <h2 class="system">SYSTEM <span>NU BICYCLE</span></h2>
+                        </div>
+                        <div class="header-content">
+                            <p>ระบบยืมคืนรถจักรยานมหาวิทยาลัยนเรศวร</p>
+                        </div>
+                        <div class="header-btn">
+                            <a href="signUp.php">Sign Up</a>
+                        </div>
+                    </div>
+                    <div class="imgBox">
+                        <img src="img/bike (1).svg" alt="">
+                    </div>
+                </div>
+            </section>
+        </div>
+    </header>
+</body>
+</html>
